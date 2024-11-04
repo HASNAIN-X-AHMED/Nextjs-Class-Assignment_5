5856831649f65fbeac04639975a184a5dabd5aa6
HasnainAhamed <hasnainahmed3579@gmail.com>
2024-11-03 16:31:29 -0800

    file uplode

---
+++ app/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div>
+      <header className='bg-[#A29875]  h-[55px]'>
+        <nav>
+            <h1 className='text-[#ffffff] font-["Rye", serif] font-bold p-4 text-lg]'>MANZZARI</h1>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default Header
